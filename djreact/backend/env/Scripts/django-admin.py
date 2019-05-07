d873cfc199fb3b05b1c9b12090753b7d46ec76e1
chitru <schitru@gmail.com>
2019-05-07 22:02:15 +1000

    Django restframework and react successfully running

---
+++ djreact/backend/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\works and compositions\restapi\djreact\backend\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
